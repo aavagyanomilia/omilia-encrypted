3837f9c06aa8c2f120280b8de4ca233280f924a2
aavagyanomilia <123752512+aavagyanomilia@users.noreply.github.com>
2023-04-10 13:01:30 -0700

    Third commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', async (req, res, next) => {
 
 app.post('/omilia-encrypted', async (req, res, next) => {
     console.log(req.body);
-    res.json(req.body)
+    res.status(200).json(req.body)
 })
 
 app.listen(3000, () => {
